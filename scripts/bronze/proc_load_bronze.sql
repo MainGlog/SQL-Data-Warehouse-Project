9ddcf7c657b1a6d097bb87bdcb0d227b63c76047
Logan Brooks <loganbrooks533@gmail.com>
2025-11-25 17:35:51 -0500

    Finished Bronze Layer Loading Script
    - Changed variable names to align with the specified naming conventions of the project
    
    - Added Try... Catch Block
    - Added Time Tracker
    - Added Printouts

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -1,64 +1,94 @@
 /* Gets all the names of the tables in the bronze schema */ 
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
 BEGIN
-	DECLARE @TableNames TABLE (ID INT, Name NVARCHAR(64)); 
+	BEGIN TRY
+		DECLARE @table_names TABLE (ID INT, Name NVARCHAR(64)); 
 
-	INSERT INTO @TableNames
-	SELECT ROW_NUMBER() OVER (ORDER BY T.object_id) AS 'ID', T.name
-	FROM SYS.SCHEMAS S
-	JOIN SYS.TABLES T ON S.schema_id = T.schema_id
-	WHERE S.name = 'bronze'
-	ORDER BY T.name;
+		INSERT INTO @table_names
+		SELECT ROW_NUMBER() OVER (ORDER BY T.object_id) AS 'ID', T.name
+		FROM SYS.SCHEMAS S
+		JOIN SYS.TABLES T ON S.schema_id = T.schema_id
+		WHERE S.name = 'bronze'
+		ORDER BY T.name;
 
 
 
-	PRINT '============================================';
-	PRINT '			 Loading Bronze Layer ';
-	PRINT '============================================' + CHAR(13);;
-	DECLARE @Count INT = 1;	
+		PRINT '============================================';
+		PRINT '			 Loading Bronze Layer ';
+		PRINT '============================================' + CHAR(13);;
 
-	DECLARE @FilePathPrefix NVARCHAR(3) = ''; 
 
-	/* Iterates through the tables */
-	WHILE @Count <= 6
-	BEGIN
-		DECLARE @TableName NVARCHAR (64) = (SELECT Name FROM @TableNames WHERE ID = @Count); 
+		DECLARE @count INT = 1;	-- Loop iteration variable
+		DECLARE @file_path_prefix NVARCHAR(3) = ''; -- Whether the source is from CRM or ERP
+		DECLARE @total_time INT = 0;
 
-		/* Only prints out a new section if the section has changed. I.e. crm to erp */
-		DECLARE @TempPrefix NVARCHAR(3) = (SELECT TOP 1 * FROM STRING_SPLIT(@TableName, '_'));
-		IF @FilePathPrefix != @TempPrefix
+		/* Iterates through the tables */
+		WHILE @count <= 6
 		BEGIN
-			SET @FilePathPrefix = @TempPrefix;
+			DECLARE @table_name NVARCHAR (64) = (SELECT Name FROM @table_names WHERE ID = @count); 
 
-			PRINT '--------------------------------------------';
-			PRINT '			 Loading ' + UPPER(@FilePathPrefix) + ' Tables...';
-			PRINT '--------------------------------------------';
+			/* Only prints out a new section if the section has changed. I.e. crm to erp */
+			DECLARE @temp_prefix NVARCHAR(3) = (SELECT TOP 1 * FROM STRING_SPLIT(@table_name, '_'));
+			IF @file_path_prefix != @temp_prefix
+			BEGIN
+				SET @file_path_prefix = @temp_prefix;
+
+				PRINT '=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=';
+				PRINT '			 Loading ' + UPPER(@file_path_prefix) + ' Tables...';
+				PRINT '=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=' + CHAR(13);
+			END
+
+			/* Obtains the file name of the data source */
+			DECLARE @file_name NVARCHAR(32) = (SELECT SUBSTRING(@table_name, CHARINDEX('_', @table_name) + 1, LEN(@table_name)) + '.csv');
+			
+			/* Truncates the table before bulk loading the data from the file source into the appropriate table */
+			DECLARE @statement NVARCHAR(MAX) = '
+			PRINT ''--------------------------------------------'';
+
+			PRINT ''>> Truncating Table: bronze.' + @table_name + ''';
+			
+
+
+			TRUNCATE TABLE bronze.' + @table_name + ';
+
+			PRINT ''>> Inserting Data Into: bronze.' + @table_name + ''';
+
+			BULK INSERT bronze.' + @table_name + '
+			FROM ''C:\Users\mtnje\OneDrive\Desktop\Programming\Database Stuff\SQL Data Warehouse Project\datasets\source_' + @file_path_prefix + '\' + @file_name + '''
+			WITH (
+				FIRSTROW = 2, 
+				FIELDTERMINATOR = '','',
+				TABLOCK 
+			);
+			';
+
+			DECLARE @start_time DATETIME, @end_time DATETIME, @difference INT;
+			
+			SET @start_time = GETDATE();
+			
+			EXEC(@statement);
+			
+			SET @end_time = GETDATE();
+			SET @difference = DATEDIFF(second, @start_time, @end_time);
+			SET @total_time = @total_time + @difference;
+			
+			PRINT '>> Load Duration: ' + CAST(@difference AS NVARCHAR) + ' seconds';
+			PRINT '--------------------------------------------' + CHAR(13);
+			
+			SET @count = @count + 1;
 		END
+		PRINT '============================================';
+		PRINT '		Bronze Layer Loaded in ' + CAST(@total_time AS NVARCHAR) + ' seconds';
+		PRINT '============================================' + CHAR(13);;
+	END TRY
 
-		/* Obtains the file name of the data source */
-		DECLARE @FileName NVARCHAR(32) = (SELECT SUBSTRING(@TableName, CHARINDEX('_', @TableName) + 1, LEN(@TableName)) + '.csv');
-	
-		/* Truncates the table before bulk loading the data from the file source into the appropriate table */
-		DECLARE @Statement NVARCHAR(MAX) = '
-		PRINT ''>> Truncating Table: bronze.' + @TableName + ''';
-	
-		TRUNCATE TABLE bronze.' + @TableName + ';
-
-		PRINT ''>> Inserting Data Into: bronze.' + @TableName + ''';
-
-		BULK INSERT bronze.' + @TableName + '
-		FROM ''C:\Users\mtnje\OneDrive\Desktop\Programming\Database Stuff\SQL Data Warehouse Project\datasets\source_' + @FilePathPrefix + '\' + @FileName + '''
-		WITH (
-			FIRSTROW = 2, 
-			FIELDTERMINATOR = '','',
-			TABLOCK 
-		);
-
-		PRINT '''';
-		';
-
-		EXEC(@Statement);
-
-		SET @Count = @Count + 1;
-	END
+	BEGIN CATCH
+		PRINT '============================================';
+		PRINT '  ERROR OCCURED DURING LOADING BRONZE LAYER';
+		PRINT 'Error Message: ' + ERROR_MESSAGE();
+		PRINT 'Error Number: ' + CAST(ERROR_NUMBER() AS NVARCHAR);
+		PRINT 'Error State: ' + CAST(ERROR_STATE() AS NVARCHAR);
+		PRINT '============================================';
+	END CATCH
 END
+
